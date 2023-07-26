fb1cd06f90b566cedda9fde57937a4745e23b857
mark <mehdimirzaie1378@gmail.com>
2023-07-26 16:34:13 +0330

    serializers.py | created for user and product

---
+++ backend/base/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Product
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            '_id', 'user', 'name', 'image', 'brand', 'category',
+            'description', 'rating', 'numReviews', 'price',
+        ]
+
+    user = serializers.StringRelatedField()
