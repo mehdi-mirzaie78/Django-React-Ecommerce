d8261614eb39ce0ba704e6419c87af042bb4778c
MARK <mehdimirzaie1378@gmail.com>
2024-02-02 15:09:12 +0330

    modified product details with redux,reducers and actions

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -6,15 +6,25 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username']
+        fields = ["username"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            '_id', 'user', 'name', 'image', 'brand', 'category',
-            'description', 'rating', 'numReviews', 'price',
+            "_id",
+            "user",
+            "name",
+            "image",
+            "brand",
+            "category",
+            "description",
+            "rating",
+            "numReviews",
+            "price",
+            "countInStock",
+            "reviews",
         ]
 
     user = serializers.StringRelatedField()
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,82 +1,88 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from "axios";
+import { Loader } from "../components/Loader";
+import { Message } from "../components/Message";
+import { detailsProduct } from "../actions/productActions";
+import { useDispatch, useSelector } from "react-redux";
 
 function ProductScreen() {
-  const params = useParams()
-  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { error, loading, product } = productDetails;
   useEffect(() => {
-    async function fetchProduct() {
-    const { data } = await axios.get(`/api/products/${params.id}`);
-      console.log(data)
-      setProduct(data);
-    }
-    fetchProduct();
-  });
+    dispatch(detailsProduct(id));
+  }, [dispatch, id]);
 
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
         Go Back
       </Link>
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid rounded />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant="flush" className="rounded">
-            <ListGroup.Item className="border">
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item className="border">
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-                color={"#f8e825"}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item className="border">
-              Price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item className="border">
-              Description: {product.description}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid rounded />
+          </Col>
+          <Col md={4}>
             <ListGroup variant="flush" className="rounded">
               <ListGroup.Item className="border">
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item className="border">
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                  color={"#f8e825"}
+                />
               </ListGroup.Item>
-              <ListGroup.Item className="text-center">
-                <Button
-                  className="btn-block w-100"
-                  disabled={product.countInStock === 0}
-                  type="button"
-                >
-                  Add To Cart
-                </Button>
+              <ListGroup.Item className="border">
+                Price: ${product.price}
+              </ListGroup.Item>
+              <ListGroup.Item className="border">
+                Description: {product.description}
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush" className="rounded">
+                <ListGroup.Item className="border">
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item className="border">
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item className="text-center">
+                  <Button
+                    className="btn-block w-100"
+                    disabled={product.countInStock === 0}
+                    type="button"
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 }
