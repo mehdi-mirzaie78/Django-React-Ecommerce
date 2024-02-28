e5896b2c108dae9b0713dcef3b4ac8658d309be0
specter <mehdimirzaie1378@gmail.com>
2024-02-28 11:48:37 +0330

    frontend | ShippingScreen.js | added checkout steps

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { FormContainer } from "../components/FormContainer";
+import { CheckoutSteps } from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 
 const ShippingScreen = () => {
@@ -23,54 +24,57 @@ const ShippingScreen = () => {
   };
 
   return (
-    <FormContainer>
-      <h1>Shipping</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId="country" className="my-2">
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Enter country"
-            value={country ? country : ""}
-            onChange={(e) => setCountry(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="city" className="my-2">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Enter city"
-            value={city ? city : ""}
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="postalCode" className="my-2">
-          <Form.Label>Postal Code</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Enter postal code"
-            value={postalCode ? postalCode : ""}
-            onChange={(e) => setPostalCode(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="address" className="my-2">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Enter address"
-            value={address ? address : ""}
-            onChange={(e) => setAddress(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Button type="submit" variant="primary" className="my-3">
-          Continue
-        </Button>
-      </Form>
-    </FormContainer>
+    <>
+      <CheckoutSteps step1 step2 />
+      <FormContainer>
+        <h1>Shipping</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="country" className="my-2">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter country"
+              value={country ? country : ""}
+              onChange={(e) => setCountry(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="city" className="my-2">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter city"
+              value={city ? city : ""}
+              onChange={(e) => setCity(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="postalCode" className="my-2">
+            <Form.Label>Postal Code</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter postal code"
+              value={postalCode ? postalCode : ""}
+              onChange={(e) => setPostalCode(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="address" className="my-2">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter address"
+              value={address ? address : ""}
+              onChange={(e) => setAddress(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary" className="my-3">
+            Continue
+          </Button>
+        </Form>
+      </FormContainer>
+    </>
   );
 };
 
