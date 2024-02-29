25c5697cde4f1eca987287678274165502b5a694
specter <mehdimirzaie1378@gmail.com>
2024-02-29 18:48:51 +0330

    backend | fixed order view

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -7,15 +7,15 @@ from .serializers import OrderSerializer
 
 
 class OrderAddView(APIView):
-    permissin_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         data = request.data
         order_items = data["orderItems"]
-        shiping_address = data["shippingAddress"]
+        shipping_address = data["shippingAddress"]
 
         order = Order.create_order_from_data(request.user, data)
-        ShippingAddress.create_shipping_address_from_data(order, shiping_address)
+        ShippingAddress.create_shipping_address_from_data(order, shipping_address)
         order.create_order_items_and_update_stock(order_items)
 
         serializer = OrderSerializer(order)
