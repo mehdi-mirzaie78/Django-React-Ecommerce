203e73f5dfa41b4fce01ee458fe9719aacb4f65c
specter <mehdimirzaie1378@gmail.com>
2024-03-04 15:56:50 +0330

    backend | new endpoint to update order payment status

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("add/", views.OrderAddView.as_view(), name="order-add"),
-    path("<int:pk>/", views.OrderDetailView.as_view(), name="order-detail")
+    path("<int:pk>/", views.OrderDetailView.as_view(), name="order-detail"),
+    path("<int:pk>/pay/", views.OrderUpdateToPaid.as_view(), name="order-pay"),
 ]
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,5 @@
+from pytz import timezone
+from datetime import datetime
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
@@ -39,3 +41,12 @@ class OrderDetailView(APIView):
         order = self.get_obj(pk)
         serializer = OrderSerializer(order)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class OrderUpdateToPaid(APIView):
+    def get(self, request, pk):
+        queryset = Order.objects.filter(pk=pk)
+        if not queryset.exists():
+            raise NotFound(detail="Order Not Found")
+        queryset.update(is_paid=True, paid_at=datetime.now(timezone("Asia/Tehran")))
+        return {"msg": f"Order {pk} updated successfully"}
