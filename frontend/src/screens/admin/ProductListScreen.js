4286170e78e01ff03b3a5693b8639d8ec46ed211
specter <mehdimirzaie1378@gmail.com>
2024-03-08 13:40:46 +0330

    frontend | react admin panel | added product list screen

--- frontend/src/screens/admin/ProductListScreen.js
+++ frontend/src/screens/admin/ProductListScreen.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
-import { Table, Button, Row, Col } from "react-bootstrap";
+import { Table, Button, Row, Col, Image } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Loader } from "../../components/Loader";
 import { Message } from "../../components/Message";
-import { listProducts } from "../../actions/productActions";
+import {
+  getProductAdminList,
+  deleteProductAdmin,
+} from "../../actions/productActions";
 
 const ProductListScreen = () => {
   const dispatch = useDispatch();
@@ -15,21 +18,27 @@ const ProductListScreen = () => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const productAdminList = useSelector((state) => state.productAdminList);
+  const { loading, error, products } = productAdminList;
 
+  const productAdminDelete = useSelector((state) => state.productAdminDelete);
+  const {
+    loading: loadingProductDelete,
+    error: errorProductDelete,
+    success: successDelete,
+  } = productAdminDelete;
   useEffect(() => {
     if (!userInfo) {
       navigate(`/login?redirect=${location.pathname.substring(1)}`);
     } else if (!userInfo.isAdmin) {
       navigate("/");
     }
-    dispatch(listProducts());
-  }, [dispatch, navigate, location, userInfo]);
+    dispatch(getProductAdminList());
+  }, [dispatch, navigate, location, userInfo, successDelete]);
 
   const deleteHandler = (productId) => {
-    if (window.confirm("Are you sure you want to delete this user?")) {
-      //   dispatch(deleteProduct(productId));
+    if (window.confirm("Are you sure you want to delete this product?")) {
+      dispatch(deleteProductAdmin(productId));
     }
   };
   const createProductHandler = (product) => {
@@ -47,7 +56,12 @@ const ProductListScreen = () => {
           </Button>
         </Col>
       </Row>
-      {loading ? (
+
+      {errorProductDelete && (
+        <Message variant="danger">{errorProductDelete}</Message>
+      )}
+
+      {loading || loadingProductDelete ? (
         <Loader />
       ) : error ? (
         <Message variant="danger">{error}</Message>
@@ -64,8 +78,10 @@ const ProductListScreen = () => {
               <th>ID</th>
               <th>NAME</th>
               <th>PRICE</th>
+              <th>STOCK</th>
               <th>CATEGORY</th>
               <th>BRAND</th>
+              <th>IMAGE</th>
               <th>ACTIONS</th>
             </tr>
           </thead>
@@ -75,8 +91,13 @@ const ProductListScreen = () => {
                 <td>{product.id}</td>
                 <td>{product.name}</td>
                 <td>$ {product.price}</td>
+                <td>{product.countInStock}</td>
                 <td>{product.category}</td>
                 <td>{product.brand}</td>
+                <td className="py-1">
+                  <Image height={50} className="rounded" src={product.image} />
+                </td>
+
                 <td>
                   <LinkContainer
                     to={`/admin/product/${product.id}`}
