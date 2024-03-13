483a5460a8f3b92853195f40c1ce05fb6756124d
specter <mehdimirzaie1378@gmail.com>
2024-03-13 12:04:33 +0330

    backend | products | views.py | updated with search functionality

--- backend/products/views/views.py
+++ backend/products/views/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -13,6 +14,12 @@ from ..serializers import ProductSerializer, ReviewSerializer
 class ProductListView(APIView):
     def get(self, request):
         queryset = Product.objects.all()
+        if search := request.query_params.get("search"):
+            queryset = queryset.filter(
+                Q(name__icontains=search)
+                | Q(brand__icontains=search)
+                | Q(category__icontains=search)
+            )
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
